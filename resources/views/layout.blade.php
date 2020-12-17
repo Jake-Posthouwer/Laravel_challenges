02b81a7fa9491bd5c06c695fa0ef28b126f74e59
533301 <533301@student.glu.nl>
2020-12-17 11:45:54 +0100

    challenge 2

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Layout</title>
+    </head>
+    <body>
+
+        @yield('content')
+        <nav>test</nav>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/about">about</a>
+            </li>
+            <li>
+                <a href="/work">work</a>
+            </li>
+            <li>
+                <a href="/contact">Contact</a>
+            </li>
+        </ul>
+
+        <footer></footer>
+    </body>
+</html>
